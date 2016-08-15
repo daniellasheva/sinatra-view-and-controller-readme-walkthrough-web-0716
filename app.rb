2c12731051ba25b00e5b29aaa36e456eaa20cddb
daniellasheva <daniellasheva@gmail.com>
2016-08-15 10:54:54 -0400

    initial commit

--- app.rb
+++ app.rb
@@ -4,14 +4,19 @@ class App < Sinatra::Base
 
   get '/reverse' do
     erb :reverse
+    #this method sends data to the post '/reverse' route
   end
 
-  post '/reverse' do
-    erb :reversed
+  post '/reverse' do  #two different pages are rendered here
+    #This method gets data from the get '/reverse' method
+    original_string= params["string"]
+    @reversed_string= original_string.reverse #this instance variable is now visible to the entire class
+    erb :reversed                             #including our view
   end
 
   get '/friends' do
-    #write your code here
+    @friends = ["Charlie Chaplin", "Richard Pryor", "Eddie Murphy", "Louis CK", "Jerry Seinfeld"]
+    erb :friends
   end
 
 end
